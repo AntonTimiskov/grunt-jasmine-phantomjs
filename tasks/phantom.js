6cd7c9eb243bce05d935495afcb238679e1cfc28
Anton Timiskov <anton.timiskov@software.dell.com>
2014-02-04 20:32:46 +0400

    phantom runner and reporter for jasmine tests

---
+++ tasks/phantom.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('phantom', 'Start PhantomJS for specified URL', function() {
+    var options, port, stopped, done, phantom, binPath, which;
+
+    try {
+      which = require('which');
+      binPath = which.sync('phantomjs');
+    } catch(e) {
+      binPath = require('phantomjs').path;
+    }
+
+    options = this.options({
+        runner: require('path').resolve(__dirname, '..', 'jasmine-runner.js'),
+        url: 'http://localhost:3000'
+    });
+
+    stopped = false;
+    done = this.async();
+
+    // Spawn PhantomJS.
+    phantom = grunt.util.spawn({
+      cmd: binPath,
+      args: [options.runner, options.url]
+    }, function () {
+      stopped = true;
+      grunt.fatal('PhantomJS killed unexpectedly');
+    });
+
+    // Log PhantomJS error.
+    phantom.stderr.setEncoding('utf-8');
+    phantom.stderr.on('data', function (chunk) {
+      grunt.log.error('PhantomJS: ' + chunk);
+    });
+
+    // Log PhantomJS output.
+    phantom.stdout.setEncoding('utf-8');
+    phantom.stdout.on('data', function (chunk) {
+      grunt.log.debug('PhantomJS: ' + chunk);
+    });
+
+    // Kill PhantomJS on exit.
+    process.on('exit', function () {
+      if (!stopped) {
+        phantom.kill();
+        grunt.log.ok('PhantomJS stopped');
+        done();
+      }
+    });
+
+  });
+};
